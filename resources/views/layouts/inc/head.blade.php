266aff706fa828547305d20b315971b9ff1a84c5
Ahmedpro <ahmed.6nasser@gmail.com>
2018-09-21 14:09:59 +0200

    make status seeder

--- resources/views/layouts/inc/head.blade.php
+++ resources/views/layouts/inc/head.blade.php
@@ -2,15 +2,14 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
+
       <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="description" content="">
-      <meta name="author" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="description" content="">
       <meta name="author" content="">
       <meta name="csrf-token" content="{{ csrf_token() }}">
+
       <title>Easy Shopping</title>
       <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
       <!--[ifl tablet IE 9]>
